f91032b74dd26784304c0563874e7b2bacd2511a
jperalta4 <jperalta@infodoc.com.co>
2025-07-24 17:33:52 -0500

    [feature] add pattern  retry

--- src/main/java/com/inventario/pruebaTecnica/helper/client/FeignConfig.java
+++ src/main/java/com/inventario/pruebaTecnica/helper/client/FeignConfig.java
@@ -2,10 +2,14 @@ package com.inventario.pruebaTecnica.helper.client;
 
 import feign.Request;
 import feign.RequestInterceptor;
+import feign.RetryableException;
+import feign.Retryer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
-import java.util.concurrent.TimeUnit;
+import org.springframework.context.annotation.Configuration;
 
+import java.util.concurrent.TimeUnit;
+@Configuration
 public class FeignConfig {
 
     @Value("${product.service.api-key}")
@@ -18,6 +22,16 @@ public class FeignConfig {
     @Value("${feign.client.read-timeout:10000}")
     private int readTimeout;
 
+    // Retry configurations
+    @Value("${feign.client.retry-period:1000}")
+    private long period;
+
+    @Value("${feign.client.retry-max-period:5000}")
+    private long maxPeriod;
+
+    @Value("${feign.client.retry-max-attempts:3}")
+    private int maxAttempts;
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
@@ -27,17 +41,34 @@ public class FeignConfig {
         };
     }
 
-    /**
-     * Configures timeout settings for Feign client
-     * 
-     * @return Request.Options with configured timeouts
-     */
     @Bean
     public Request.Options options() {
         return new Request.Options(
                 connectTimeout, TimeUnit.MILLISECONDS,
                 readTimeout, TimeUnit.MILLISECONDS,
-                true // Follow redirects
-        );
+                true);
     }
+
+    /**
+     * Configures retry logic for Feign client
+     */
+    @Bean
+    public Retryer retryer() {
+        return new Retryer.Default(1000, 5000, 3) {
+            private int attempt = 1;
+
+            @Override
+            public void continueOrPropagate(RetryableException e) {
+                System.out.println("🔁 Intento #" + attempt + " fallido: " + e.getMessage());
+                attempt++;
+                super.continueOrPropagate(e);
+            }
+
+            @Override
+            public Retryer clone() {
+                return new Retryer.Default(1000, 5000, 3);
+            }
+        };
+    }
+
 }
